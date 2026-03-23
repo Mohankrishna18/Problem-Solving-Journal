8cd75715d71e08562983e73a4d3e63b66463651c
Mohankrishna18 <mohankrishnamadanapu@gmail.com>
2026-03-23 12:53:51 +0530

    Day 4 DSA problem 23/03/2026

---
+++ Day4/Problem3.java
@@ -0,0 +1,27 @@
+package Day4;
+
+public class Problem3 {
+    public static void main(String[] args){
+        int[]  arr = {5, 2, 9, 1, 5, 6};
+
+        // Initialize min and max with the first element
+        int max = arr[0];
+        int min = arr[0];
+
+        // Traverse the array
+
+        for(int i=1;i < arr.length;i++){
+            if(arr[i] < min){
+                min = arr[i];
+            }
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Max value : " + max);
+        System.out.println("Min value : "+min);
+
+
+    }
+}
