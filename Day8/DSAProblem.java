a4a8b0f638b47a1777778575b432687968987caa
Mohankrishna18 <mohankrishnamadanapu@gmail.com>
2026-03-27 13:14:26 +0530

    comments added

--- Day8/DSAProblem.java
+++ Day8/DSAProblem.java
@@ -1,12 +1,16 @@
 package Day8;
 
+
+ // Bubble Sort algorithm
 public class DSAProblem {
     public static void main(String[] args){
         int[] arr = {64, 34, 25, 12, 22, 11, 90};
 
         int n = arr.length;
+        // Bubble Sort algorithm
         for(int i = 0; i < n - 1; i++){
             for(int j = 0; j < n - i - 1; j++){
+                // Swap if the element is greater than the next
                 if(arr[j] > arr[j + 1]){
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
