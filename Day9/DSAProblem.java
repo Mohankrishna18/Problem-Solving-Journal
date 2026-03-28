d12abe8e58d2acdb6e2d2c3628699c2afb79e6af
Mohankrishna18 <mohankrishnamadanapu@gmail.com>
2026-03-28 10:35:42 +0530

    Day 9 - Encapsulation, Streams filter+sort, Selection sort

---
+++ Day9/DSAProblem.java
@@ -0,0 +1,30 @@
+package Day9;
+
+public class DSAProblem {
+
+    public static void selectionSort(int[] arr){
+        int len = arr.length;
+
+        for(int i = 0;i < len-1; i++){
+            int minId = i;
+            for(int j = i+1; j < len ;j++){
+                if(arr[j] < arr[minId]){
+                        minId = j;
+                }
+        }
+        int temp = arr[minId];
+            arr[minId] = arr[i];
+            arr[i] = temp;
+    }
+    }
+
+    public static void main(String[] args){
+        int[] arr = {64, 25, 12, 22, 110};
+
+        selectionSort(arr);
+        for(int num : arr){
+            System.out.print(num + " ");
+        }
+    }
+    
+}
