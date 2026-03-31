6458345c3d0892b26e8141b4eb23004c4fa43c4f
Mohankrishna18 <mohankrishnamadanapu@gmail.com>
2026-03-31 19:53:43 +0530

    merge sort

--- Day11/MergeSort.java
+++ Day11/MergeSort.java
@@ -1,5 +1,77 @@
 package Day11;
 
-public class MergeSort {
-    
+class MergeSort {
+    // Merge two halves
+    void merge(int arr[], int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        // Copy data
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[left + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0;
+        int k = left;
+
+        // Merge temp arrays
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    // Sort function
+    void sort(int arr[], int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+
+            sort(arr, left, mid);
+            sort(arr, mid + 1, right);
+
+            merge(arr, left, mid, right);
+        }
+    }
+
+    // Print array
+    static void printArray(int arr[]) {
+        for (int i : arr)
+            System.out.print(i + " ");
+        System.out.println();
+    }
+
+    // Driver
+    public static void main(String args[]) {
+        int arr[] = {38, 27, 43, 3, 9, 82, 10};
+        System.out.println("Given Array:");
+        printArray(arr);
+
+        MergeSort ob = new MergeSort();
+        ob.sort(arr, 0, arr.length - 1);
+
+        System.out.println("\nSorted Array:");
+        printArray(arr);
+    }
 }
