a424b5a6674212e651a9906f6713851027b1a407
Mohankrishna18 <mohankrishnamadanapu@gmail.com>
2026-04-04 20:02:07 +0530

    summaryStatistics

--- Day16/StreamApi.java
+++ Day16/StreamApi.java
@@ -1,3 +1,19 @@
+import java.util.*;
+import java.util.stream.*;
+
 public class StreamApi {
-    
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(10, 20, 30, 40, 50);
+        // Use summarizingInt to get statistics
+        IntSummaryStatistics stats = numbers.stream()
+                                    .mapToInt(Integer::intValue)
+                                    .summaryStatistics();
+
+        // Print results
+        System.out.println("Count = " + stats.getCount());
+        System.out.println("Sum = " + stats.getSum());
+        System.out.println("Min = " + stats.getMin());
+        System.out.println("Average = " + stats.getAverage());
+        System.out.println("Max = " + stats.getMax());
+    }
 }
